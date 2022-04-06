f2c5b2e503fb6f6e40c51a2fee1efa4b0b599ef0
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-04-06 12:35:58 +0200

    Created Image Screen boilerplate.

---
+++ rn-starter/src/screens/ImageScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ImageScreen = () => {
+    return (
+        <View>
+            <Text>ImageScreen</Text>
+        </View>
+    );
+};
+
+export default ImageScreen;
