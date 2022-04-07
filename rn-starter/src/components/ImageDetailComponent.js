2d1fcd3bbb7c6c50c1692b885661eb062b96c640
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-04-07 03:15:40 +0200

    Added styles to Text

--- rn-starter/src/components/ImageDetailComponent.js
+++ rn-starter/src/components/ImageDetailComponent.js
@@ -1,12 +1,17 @@
 import {View,Text,StyleSheet,Image} from 'react-native';
 import React from 'react';
-const ImageDetail = ({title,image}) => {
+const ImageDetail = ({title,image,score}) => {
     return (
         <View>
             <Image source={image}/>
-            <Text>{title}</Text>
+            <Text style={styles.textStyle}>Image Title -{title}</Text>
+            <Text style={styles.textStyle}> Image Score -{score}</Text>
         </View>
     );
 };
-
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize:30
+    }
+})
 export default ImageDetail;
