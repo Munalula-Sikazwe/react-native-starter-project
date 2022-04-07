388ab845ba1a0403248631c83f0fbce9242c429a
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2022-04-07 03:09:44 +0200

    Added score to data objects.

--- rn-starter/src/screens/ImageScreen.js
+++ rn-starter/src/screens/ImageScreen.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import {View,Text,StyleSheet,FlatList} from 'react-native';
+import {FlatList} from 'react-native';
 import ImageDetail from "../components/ImageDetailComponent";
+
 const ImageScreen = () => {
     const data = [
-        {title:"Forest",image: require("../../assets/images/forest.jpg")},
-        {title:"Beach",image:require("../../assets/images/beach.jpg")},
-        {title:"Mountain",image:require("../../assets/images/mountain.jpg")}
+        {title: "Forest", image: require("../../assets/images/forest.jpg"), score: 9},
+        {title: "Beach", image: require("../../assets/images/beach.jpg"), score: 7},
+        {title: "Mountain", image: require("../../assets/images/mountain.jpg"), score: 4}
     ]
     return (
         <FlatList
-        data={data}
-        keyExtractor={(data)=>data.title}
-        renderItem={({item})=><ImageDetail title={item.title} image={item.image}/>
+            data={data}
+            keyExtractor={(data) => data.title}
+            renderItem={({item}) => <ImageDetail title={item.title} image={item.image}/>
 
-       }
+            }
 
         >
 
